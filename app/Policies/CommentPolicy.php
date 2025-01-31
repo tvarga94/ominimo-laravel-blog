2d7f01c5cdd2f41243b3213ba496348ca605b530
Varga Tamas <vargata1@gmail.com>
2025-01-31 15:30:52 +0100

    Policies added

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CommentPolicy
+{
+    public function delete(User $user, Comment $comment): bool
+    {
+        return $user->id === $comment->user_id || $user->id === $comment->post->user_id;
+    }
+}
