b84be3c641ad1791ab6a53a3b5598a8d99571863
Varga Tamas <vargata1@gmail.com>
2025-02-02 14:27:46 +0100

    route and migration file fix

--- database/migrations/2025_02_02_114727_add_role_to_users_table.php
+++ database/migrations/2025_02_02_114727_add_role_to_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user')->after('email'); // Default role is 'user'
+            $table->string('role')->default('user')->after('email')->nullable();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use App\Http\Middleware\RedirectAfterLogin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
     ->middleware(RedirectAfterLogin::class);
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
@@ -18,7 +20,7 @@ Route::get('/dashboard', function () {
     return redirect()->route('posts.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Profile routes (protected)
+
 Route::middleware('auth')->group(function () {
     Route::controller(ProfileController::class)->group(function () {
         Route::get('/profile', 'edit')->name('profile.edit');
@@ -31,9 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::resource('posts', PostController::class)->except(['index', 'show']);
     Route::post('/posts/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
-
 });
 
-Route::resource('posts', PostController::class)->only(['index', 'show']);
+Route::resource('posts', PostController::class)->only(['show']);
 
 require __DIR__.'/auth.php';
